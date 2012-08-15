181a194c2b26fd6905a0f9c6bb9d33a8c4dc58f9
Benjamin Marten <Ben@Benjamin-Martens-MacBook-Pro.local>
2012-08-15 19:32:12 +0200

    Initial Commit

---
+++ CloudTagger/CloudTaggerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CloudTaggerAppDelegate.h
+//  CloudTagger
+//
+//  Created by Benjamin Marten on 15.08.12.
+//  Copyright (c) 2012 Benjamin Marten. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CloudTaggerAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CloudTagger/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CloudTagger
+//
+//  Created by Benjamin Marten on 15.08.12.
+//  Copyright (c) 2012 Benjamin Marten. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
